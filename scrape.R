31a083790ca660c40da045034abf94e63634e011
schochastics <david@schochastics.net>
2023-11-02 19:03:07 +0100

    restart

--- scrape.R
+++ scrape.R
@@ -1,10 +1,13 @@
+url <- "https://services.gis.konstanz.digital/geoportal/rest/services/Fachdaten/Parkplaetze_Parkleitsystem/MapServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json"
 while (TRUE) {
-    tmp <- jsonlite::fromJSON("https://services.gis.konstanz.digital/geoportal/rest/services/Fachdaten/Parkplaetze_Parkleitsystem/MapServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json")
-    df <- jsonlite::flatten(tmp$features)
-    df$attributes.opening_h <- NULL
-    df$attributes.fee_descr <- NULL
-    df$time <- Sys.time()
-    readr::write_csv(df, "parking.csv", append = file.exists("parking.csv"))
-    cat("written at", as.character(Sys.time()), "\n")
+    tmp <- tryCatch(jsonlite::fromJSON(url), error = function(e) data.frame())
+    if (length(tmp) != 0) {
+        df <- jsonlite::flatten(tmp$features)
+        df$attributes.opening_h <- NULL
+        df$attributes.fee_descr <- NULL
+        df$time <- Sys.time()
+        readr::write_csv(df, "parking.csv", append = file.exists("parking.csv"))
+        cat("written at", as.character(Sys.time()), "\n")
+    }
     Sys.sleep(5 * 60)
 }
